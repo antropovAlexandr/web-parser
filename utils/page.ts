78637cf57dd7f396a9927e467a6e79cf7a370c14
Aliaksandr Antropau <aliaksandr.antropau@inspirebrands.com>
2023-05-07 20:52:21 +0400

    add cache

--- utils/page.ts
+++ utils/page.ts
@@ -1,8 +1,9 @@
 import puppeteer, { Browser, Page } from 'puppeteer'
 
 export const getBrowserInstance = () => {
-    return puppeteer.launch()
+    return puppeteer.launch({ headless: 'new' })
 }
+
 export const pageInstance = async (browser: Browser): Promise<Page> => {
     const page = await browser.newPage()
     await page.goto('https://www.linkedin.com/')
